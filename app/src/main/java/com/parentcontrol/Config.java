a86363ec92fac45e502372c3e61b963959f11536
Solo188 <133103240+Solo188@users.noreply.github.com>
2026-03-22 23:16:01 +0300

    Update Config.java

--- app/src/main/java/com/parentcontrol/Config.java
+++ app/src/main/java/com/parentcontrol/Config.java
@@ -2,5 +2,5 @@ package com.parentcontrol;
 
 public class Config {
     // Вставь сюда токен бота
-    public static final String BOT_TOKEN = "8388799545:AAGPwGKOTs47C29s6PUDFsqZbAjNh9wdrgE";
+    public static final String BOT_TOKEN = "8746699467:AAG0ein9fM92mMQDiL-1dl_16bCCkPpKVRU";
 }
